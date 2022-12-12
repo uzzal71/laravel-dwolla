a835c35522d0d6c503d5756363a0cecf05e0de0d
uzzal71 <uzzalroy.acm@gmail.com>
2022-12-13 00:18:49 +0600

    Dwolla autoload

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -23,6 +23,18 @@ class CustomerController extends Controller
      */
     public function index()
     {
+        DwollaSwagger\Configuration::$username = 'rjttGTGBueIue5bmQWTJXGy8ou0jChlbbjh2F9NiwlKBii6Qfq';
+        DwollaSwagger\Configuration::$password = 'jk7yJYORje2pE6Yfxjiuq7ZFYk8cRcq9UXhzfVfsNGuoayVgkG';
+
+        // For Sandbox
+        $apiClient = new DwollaSwagger\ApiClient("https://api-sandbox.dwolla.com");
+
+        $customersApi = new DwollaSwagger\CustomersApi($apiClient);
+
+        $customers = $customersApi->_list(10, 0);
+        
+        return $customers;
+
         return view('customer.index');
     }
 }
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
+
+    protected $table = "customers";
+    protected $fillable = ['firstName', 'lastName', 'phone', 'email', 'type', 'address1', 'address2', 'city', 'state', 'postalCode', 'dateOfBirth', 'ssn'];
 }
